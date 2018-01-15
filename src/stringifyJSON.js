9d6488d0b5829c75b67d38b5f38a870082fa384f
leungnamson1023 <leungnamson@gmail.com>
2018-01-15 12:28:27 -0800

    "Stringify passes until Objects, spec file updated"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,57 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var arrayVals = [];
+  var objArray = [];
+  //primitive types
+  if (typeof obj === 'boolean' || obj === null || typeof obj === 'number') {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    if (obj.length < 1) {
+      return '[]';
+    } else {
+      obj.forEach(function(item) {
+        arrayVals.push(stringifyJSON(item)); 
+      }); 
+      return '[' + arrayVals + ']';
+    }
+  }
+  //objects
+
+  if (obj instanceof Object) {
+    //if empty obj
+    // keys in obj to string
+    var arrOfKeys = Object.keys(obj);
+    console.log(arrOfKeys);
+    if (arrOfKeys.length === 0 ) {
+      return '{}';
+    } 
+
+    for (var key in obj) {
+    console.log(key);
+      if (key === 'string') {
+        objArray.push('"' + key + '":' + stringifyJSON(obj[key]));
+      } else if (typeof key === 'boolean' || key === null || typeof key === 'number') {
+        objArray.push('' + key + ':' + stringifyJSON(obj[key]));
+      }
+
+      return '{' + objArray + '}';
+      // objArray.push(stringifyJSON(obj[key]));
+      // objArray.push(stringifyJSON(key));
+    }
+  }
+  
+
+
+  // if parameter is array, depending on the value of each element, 
+  //run it back into the function and string it. return stringify array after
+
+
+  // if it is an object, take each key and string it, take each property and string it, return object stringed.
 };
+
+
+
+
