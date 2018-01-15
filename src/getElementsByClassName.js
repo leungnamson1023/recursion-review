e3400eae2a2986d7d5feaad58b260ffd332e04af
leungnamson1023 <leungnamson@gmail.com>
2018-01-15 15:54:22 -0800

    "Passed tests for getElementsByClassName and fixed Fill ME IN in spec file"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var matches = [];
+  var elements = document.body;
+  function traverse(node) { 
+    if (node.classList) {
+      if (node.classList.contains(className)) {
+        matches.push(node);
+      }
+    }
+    node.childNodes.forEach(function(child) {
+      traverse(child);
+    });
+  }
+
+  traverse(elements);
+  return matches;
 };
+  
+
+  // console.log(matches);
+  // return matches;
